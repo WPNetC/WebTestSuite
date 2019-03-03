0dc18aee10c03c3f80b6fe78ab0cbd0d920f1451
SidJames <sidsoft2014@outlook.com>
2019-03-03 16:36:18 +0000

    initial commit

---
+++ WebTestSuite/StandardTests/AccessTests.cs.pp
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+
+namespace $rootnamespace$.StandardTests
+{
+    [TestClass]
+    public class AccessTests : WebTestBase
+    {
+        [TestMethod]
+        public void CanAccessSite()
+        {
+            var uri = new System.Uri($"http://{PathsAndPorts.BASE_ADDR}");
+            Assert.IsTrue(TryUri(uri), $"Cannot access site on http at: {uri.AbsoluteUri}");
+        }
+
+        [TestMethod]
+        public void CanAccessSiteHttps()
+        {
+            var uri = new System.Uri($"https://{PathsAndPorts.BASE_ADDR}");
+            using (var httpClientHandler = new HttpClientHandler())
+            {
+                // This is to ignore SSL cert errors that are likely on development machines
+                httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+                using (var client = new HttpClient(httpClientHandler))
+                {
+                    var response = client.GetAsync(uri).Result;
+                    Assert.IsTrue(response.StatusCode == HttpStatusCode.OK, $"Cannot access site on https at: {uri.AbsoluteUri}");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void CanAccessRobotsTxt()
+        {
+            var uri = new System.Uri($"http://{PathsAndPorts.BASE_ADDR}{PathsAndPorts.ROBOTS_ADR}");
+            Assert.IsTrue(TryUri(uri), $"Cannot access robots.txt at: {uri.AbsoluteUri}");
+        }
+
+        [TestMethod]
+        public void CanAccessSitemap()
+        {
+            var uri = new System.Uri($"http://{PathsAndPorts.BASE_ADDR}{PathsAndPorts.SITEMAP_ADR}");
+            Assert.IsTrue(TryUri(uri), $"Cannot access sitemap at: {uri.AbsoluteUri}");
+        }
+    }
+}
